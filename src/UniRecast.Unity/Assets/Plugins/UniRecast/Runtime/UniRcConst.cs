cb0276ac320a648808b98bfab9d349999a106ab7
ikpil <ikpil0@gmail.com>
2023-12-10 13:47:59 +0900

    typo

--- src/UniRecast.Unity/Assets/Plugins/UniRecast/Runtime/UniRcConst.cs
+++ src/UniRecast.Unity/Assets/Plugins/UniRecast/Runtime/UniRcConst.cs
@@ -2,7 +2,7 @@
 {
     public static class UniRcConst
     {
-        public const string Version = "v2023.07.29";
+        public const string Version = "2023.07.29";
         public const string Tag = "NavMesh";
     }
 }
