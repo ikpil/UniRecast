edb1186012bb1731d4c43fba1dc4f90f8c4bb5f2
ikpil <ikpil@naver.com>
2024-04-11 23:47:37 +0900

    update buffers

--- Runtime/DotRecast.Core/Buffers/RcRentedArray.cs
+++ Runtime/DotRecast.Core/Buffers/RcRentedArray.cs
@@ -6,7 +6,7 @@ namespace DotRecast.Core.Buffers
 {
     public static class RcRentedArray
     {
-        public static RcRentedArray<T> RentDisposableArray<T>(int minimumLength)
+        public static RcRentedArray<T> Rent<T>(int minimumLength)
         {
             var array = ArrayPool<T>.Shared.Rent(minimumLength);
             return new RcRentedArray<T>(ArrayPool<T>.Shared, array, minimumLength);
@@ -17,44 +17,41 @@ namespace DotRecast.Core.Buffers
     {
         private ArrayPool<T> _owner;
         private T[] _array;
-        private readonly RcAtomicInteger _disposed;
 
         public int Length { get; }
+        public bool IsDisposed => null == _owner || null == _array;
 
         internal RcRentedArray(ArrayPool<T> owner, T[] array, int length)
         {
             _owner = owner;
             _array = array;
             Length = length;
-            _disposed = new RcAtomicInteger(0);
         }
 
-        public T this[int index]
+        public ref T this[int index]
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get
             {
                 RcThrowHelper.ThrowExceptionIfIndexOutOfRange(index, Length);
-                return _array[index];
+                return ref _array[index];
             }
+        }
 
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set
-            {
-                RcThrowHelper.ThrowExceptionIfIndexOutOfRange(index, Length);
-                _array[index] = value;
-            }
+        public T[] AsArray()
+        {
+            return _array;
         }
 
 
         public void Dispose()
         {
-            if (1 != _disposed.IncrementAndGet())
-                return;
-
-            _owner?.Return(_array, true);
-            _array = null;
-            _owner = null;
+            if (null != _owner && null != _array)
+            {
+                _owner.Return(_array, true);
+                _owner = null;
+                _array = null;
+            }
         }
     }
 }
