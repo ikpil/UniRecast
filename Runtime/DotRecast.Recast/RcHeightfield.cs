a138d4befaf0038e1bc9d2a33d37e4d80156d013
ikpil <ikpil@naver.com>
2024-04-29 23:42:27 +0900

    latest

--- Runtime/DotRecast.Recast/RcHeightfield.cs
+++ Runtime/DotRecast.Recast/RcHeightfield.cs
@@ -22,29 +22,21 @@ using DotRecast.Core.Numerics;
 
 namespace DotRecast.Recast
 {
-    /** Represents a heightfield layer within a layer set. */
+    /// A dynamic heightfield representing obstructed space.
+    /// @ingroup recast
     public class RcHeightfield
     {
-        /** The width of the heightfield. (Along the x-axis in cell units.) */
-        public readonly int width;
+        public readonly int width; //< The width of the heightfield. (Along the x-axis in cell units.)
+        public readonly int height; //< The height of the heightfield. (Along the z-axis in cell units.)
+        public readonly RcVec3f bmin; //< The minimum bounds in world space. [(x, y, z)]
+        public RcVec3f bmax; //< The maximum bounds in world space. [(x, y, z)]
+        public readonly float cs; //< The size of each cell. (On the xz-plane.)
+        public readonly float ch; //< The height of each cell. (The minimum increment along the y-axis.)
+        public readonly RcSpan[] spans; //< Heightfield of spans (width*height).
 
-        /** The height of the heightfield. (Along the z-axis in cell units.) */
-        public readonly int height;
-
-        /** The minimum bounds in world space. [(x, y, z)] */
-        public readonly RcVec3f bmin;
-
-        /** The maximum bounds in world space. [(x, y, z)] */
-        public RcVec3f bmax;
-
-        /** The size of each cell. (On the xz-plane.) */
-        public readonly float cs;
-
-        /** The height of each cell. (The minimum increment along the y-axis.) */
-        public readonly float ch;
-
-        /** Heightfield of spans (width*height). */
-        public readonly RcSpan[] spans;
+        // memory pool for rcSpan instances.
+        public RcSpanPool pools; //< Linked list of span pools.
+        public RcSpan freelist; //< The next free span.
 
         /** Border size in cell units */
         public readonly int borderSize;
